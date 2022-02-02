8bd6fb54dd1546efb2d9b79674f7eeb24e447806
MohamedSolimann <mohamadico@gmail.com>
2022-02-02 12:08:31 +0200

    changed fixed some bugs in db functions

--- Models/user/index.js
+++ Models/user/index.js
@@ -2,33 +2,38 @@ const mongoose = require("mongoose");
 const userModel = require("./schema");
 const bcrypt = require("bcrypt");
 
-const createUser = async (user) => {
+async function createUser(user) {
   try {
-    const encryptedPassword = bcrypt.hashSync(password, 9);
+    const encryptedPassword = bcrypt.hashSync(user.password, 9);
     user.password = encryptedPassword;
-    const newUser = new userModel(user);
+    user._id = mongoose.Types.ObjectId();
+    user.createDate = new Date();
+    let newUser = new userModel(user);
     await newUser.save();
     return newUser;
-  } catch (error) {}
-};
-const getUserByEmail = async (userEmail) => {
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function getUserByEmail(userEmail) {
   try {
-    const user = await userModel.findOne({ email: userEmail });
-    return user;
-  } catch (error) {}
-};
-const userAuthentication = (email, password) => {
-  try {
-    const user = getUserByEmail(email);
+    const user = await userModel.findOne({ email: userEmail }).lean();
     if (user) {
-      const validPassword = bcrypt.compareSync(
-        password,
-        user.password
-      );
-      return validPassword;
+      return user;
     } else {
       return false;
     }
   } catch (error) {}
-};
+}
+async function userAuthentication(email, password) {
+  try {
+    const user = await getUserByEmail(email);
+    if (user) {
+      const validPassword = bcrypt.compareSync(password, user.password);
+      return user;
+    } else {
+      return false;
+    }
+  } catch (error) {}
+}
 module.exports = { createUser, userAuthentication };
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const config = require("config");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
+const userRouter = require("./Routes/user/index");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.cors({ origin: "*" });
+app.use(cors({ origin: "*" }));
+app.use("/user", userRouter);
 mongoose.connect(
   `mongodb://${config.get("DB.host")}:${config.get("DB.port")}/${config.get(
     "DB.dbName"
