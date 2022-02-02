97bad832e6edd83c67999e58277bbcc8093e8c9a
MohamedSolimann <mohamadico@gmail.com>
2022-02-02 12:57:14 +0200

    created test cases for user signin endpoints

--- Tests/e2e/user.test.js
+++ Tests/e2e/user.test.js
@@ -7,7 +7,7 @@ const { setupDB } = require("../testDbSetup");
 
 setupDB();
 
-const singupEndpointTestCases = () => {
+const signupEndpointTestCases = () => {
   it("Suppose to create new user", async () => {
     const response = await request.post("/user/signup").send({
       email: "a@a.com",
@@ -64,7 +64,58 @@ const singupEndpointTestCases = () => {
     expect(response.body.message).toBe("Invalid Info!");
   });
 };
+const signinEndpointTestCases = () => {
+  it("Suppose to signin user ", async () => {
+    let newUser = await createUser({
+      email: "a@a.com",
+      password: "123123123",
+      role: "Regular",
+    });
+    let response = await request
+      .post("/user/signin")
+      .send({ email: "a@a.com", password: "123123123" });
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Success");
+    expect(response.headers["set-cookie"].length).toBe(1);
+  });
+  it("Suppose to get signin user validation error ", async () => {
+    let newUser = await createUser({
+      email: "a@a.com",
+      password: "123123123",
+      role: "Regular",
+    });
+    let response = await request
+      .post("/user/signin")
+      .send({ email: "@a.com", password: "123123123" });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid Info!");
+  });
+  it("Suppose to get signin user validation error ", async () => {
+    let newUser = await createUser({
+      email: "a@a.com",
+      password: "123123123",
+      role: "Regular",
+    });
+    let response = await request
+      .post("/user/signin")
+      .send({ email: "", password: "123123123" });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid Info!");
+  });
+  it("Suppose to get signin user validation error ", async () => {
+    let newUser = await createUser({
+      email: "a@a.com",
+      password: "123123123",
+      role: "Regular",
+    });
+    let response = await request
+      .post("/user/signin")
+      .send({ email: "a@a.com", password: "" });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid Info!");
+  });
+};
 const restApiTestCases = () => {
-  singupEndpointTestCases();
+  signupEndpointTestCases(), signinEndpointTestCases();
 };
 describe("Testing Restful API for users", restApiTestCases);
