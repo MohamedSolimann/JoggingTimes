1ae926ea21dab8e5a477e9b7308deb3df85786eb
MohamedSolimann <mohamadico@gmail.com>
2022-02-02 12:19:51 +0200

    added validation to signup and signin endpoints

--- Routes/user/index.js
+++ Routes/user/index.js
@@ -3,31 +3,46 @@ const router = express.Router();
 const config = require("config");
 const jwt = require("jsonwebtoken");
 const { createUser, userAuthentication } = require("../../Models/user/index");
+const {
+  signinValidation,
+  signupValidation,
+  catchValidationErrors,
+} = require("../../validation/user.validation");
 
-router.post("/signup", async (req, res) => {
-  const user = req.body;
-  try {
-    const newUser = createUser(user);
-    res.status(201).json({ message: "Success", data: newUser });
-  } catch (error) {
-    res.status(500).json({ message: "Server Error" });
-  }
-});
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await userAuthentication(email, password);
-    if (user) {
-      const token = jwt.sign({ id: user._id }, config.get("secret"));
-      res.cookie("token", token);
-      res.status(200).json({ message: "Success" });
-    } else {
-      res.status(401).json({ message: "email or password in incorrect" });
+router.post(
+  "/signup",
+  signupValidation,
+  catchValidationErrors,
+  async (req, res) => {
+    const user = req.body;
+    try {
+      const newUser = createUser(user);
+      res.status(201).json({ message: "Success", data: newUser });
+    } catch (error) {
+      res.status(500).json({ message: "Server Error" });
     }
-  } catch (error) {
-    res.status(500).json({ message: "Server Error", error });
   }
-});
+);
+router.post(
+  "/signin",
+  signinValidation,
+  catchValidationErrors,
+  async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await userAuthentication(email, password);
+      if (user) {
+        const token = jwt.sign({ id: user._id }, config.get("secret"));
+        res.cookie("token", token);
+        res.status(200).json({ message: "Success" });
+      } else {
+        res.status(401).json({ message: "email or password in incorrect" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Server Error", error });
+    }
+  }
+);
 router.get("/signout", (req, res) => {
   try {
     res.clearCookie("token");
