1549eb64e4a1fb352d2f15b167564dac396d072b
MohamedSolimann <mohamadico@gmail.com>
2022-02-06 18:45:02 +0200

    added a created function that create report

---
+++ Models/report/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const reportModel = require("./schema");
+
+async function createReport(report, userId) {
+  try {
+    report._id = mongoose.Types.ObjectId();
+    report.createDate = new Date();
+    report.userId = userId;
+    let newReport = new reportModel(report);
+    await newReport.save();
+    return newReport ? true : false;
+  } catch (error) {
+    console.log(error);
+  }
+}
+module.exports = { createReport };
