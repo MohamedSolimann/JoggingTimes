1e43c90cb7721aef7d6ed22551e2150bbdad85bb
MohamedSolimann <mohamadico@gmail.com>
2022-02-08 20:28:25 +0200

    sepreated read record functions from index to createRecord

--- Models/records/index.js
+++ Models/records/index.js
@@ -1,17 +1,6 @@
-const mongoose = require("mongoose");
-const recordModel = require("./shcema");
 const { userRoleAuth } = require("../../Routes/user/middleware");
 const { getUserById } = require("../user/index");
-async function createRecord(record, userId) {
-  try {
-    const updatedRecord = udpateRecordForCreation(record, userId);
-    let newRecord = new recordModel(updatedRecord);
-    await newRecord.save();
-    return newRecord;
-  } catch (error) {
-    throw error;
-  }
-}
+
 function udpateRecordForCreation(record, userId) {
   record._id = mongoose.Types.ObjectId();
   record.createDate = new Date();
@@ -23,58 +12,7 @@ function udpateRecordForCreation(record, userId) {
   record.date = `${record.date.year}-${record.date.month}-${record.date.day}`;
   return record;
 }
-async function getRecordById(recordId, signedInUserId) {
-  try {
-    const record = await recordModel.findOne({ _id: recordId }).lean();
-    if (record) {
-      if (record.deleteDate) {
-        return "Record no longer exists!";
-      } else {
-        const userRoleAuthorization = await userRoleAuth(
-          signedInUserId,
-          record.user_id.valueOf()
-        );
-        if (userRoleAuthorization) {
-          return record;
-        } else {
-          return "User not Authorizied";
-        }
-      }
-    } else {
-      return "Record not found";
-    }
-  } catch (error) {
-    return error.kind;
-  }
-}
-async function getRecords(signedInUserId, userRole) {
-  try {
-    const signedInUser = await getUserById(signedInUserId, undefined, "Admin");
-    if (signedInUser.role === "Admin" || userRole === "Admin") {
-      records = await recordModel.find().lean();
-    } else if (signedInUser.role === "User Manager") {
-      records = await recordModel.find({ role: "Regular" }).lean();
-    } else {
-      records = await recordModel.find({ user_id: signedInUserId }).lean();
-    }
-    return records;
-  } catch (error) {}
-}
-async function getRecordsBetweenDates(userId, fromDate, toDate) {
-  try {
-    fromDate = `${fromDate.year}-${fromDate.month}-${fromDate.day}`;
-    toDate = `${toDate.year}-${toDate.month}-${toDate.day}`;
-    const records = await recordModel.find({
-      userId,
-      date: { $gte: fromDate, $lte: toDate },
-    });
-    if (records.length) {
-      return records;
-    } else {
-      return false;
-    }
-  } catch (error) {}
-}
+
 async function updateRecord(recordId, data, signedInId) {
   try {
     const record = await getRecordById(recordId, signedInId);
---
+++ Models/records/readRecord.js
@@ -0,0 +1,57 @@
+const recordModel = require("./shcema");
+const { userRoleAuth } = require("../../Routes/user/middleware");
+const { getUserById } = require("../user/index");
+
+async function getRecordById(recordId, signedInUserId) {
+  try {
+    const record = await recordModel.findOne({ _id: recordId }).lean();
+    if (record) {
+      if (record.deleteDate) {
+        return "Record no longer exists!";
+      } else {
+        const userRoleAuthorization = await userRoleAuth(
+          signedInUserId,
+          record.user_id.valueOf()
+        );
+        if (userRoleAuthorization) {
+          return record;
+        } else {
+          return "User not Authorizied";
+        }
+      }
+    } else {
+      return "Record not found";
+    }
+  } catch (error) {
+    return error.kind;
+  }
+}
+async function getRecords(signedInUserId, userRole) {
+  try {
+    const signedInUser = await getUserById(signedInUserId, undefined, "Admin");
+    if (signedInUser.role === "Admin" || userRole === "Admin") {
+      records = await recordModel.find().lean();
+    } else if (signedInUser.role === "User Manager") {
+      records = await recordModel.find({ role: "Regular" }).lean();
+    } else {
+      records = await recordModel.find({ user_id: signedInUserId }).lean();
+    }
+    return records;
+  } catch (error) {}
+}
+async function getRecordsBetweenDates(userId, fromDate, toDate) {
+  try {
+    fromDate = `${fromDate.year}-${fromDate.month}-${fromDate.day}`;
+    toDate = `${toDate.year}-${toDate.month}-${toDate.day}`;
+    const records = await recordModel.find({
+      userId,
+      date: { $gte: fromDate, $lte: toDate },
+    });
+    if (records.length) {
+      return records;
+    } else {
+      return false;
+    }
+  } catch (error) {}
+}
+module.exports = { getRecordById, getRecords, getRecordsBetweenDates };
